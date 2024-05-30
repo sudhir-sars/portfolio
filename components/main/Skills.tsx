fb37a835fc4c441233ff822561dd301c43ad7858
sudhir saraswat <sudhir.sars@gmail.com>
2024-05-30 17:45:40 +0530

    roll back to commit: ec2bee2

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,23 +1,18 @@
-import {
-  Backend_skill,
-  Frontend_skill,
-  Full_stack,
-  Other_skill,
-  Row2_skill,
-  Skill_data,
-} from "@/constants";
-import React from "react";
-import SkillDataProvider from "../sub/SkillDataProvider";
-import SkillText from "../sub/SkillText";
+import { Row2_skill, Skill_data } from '@/constants';
+import React from 'react';
+import SkillDataProvider from '../sub/SkillDataProvider';
+import SkillText from '../sub/SkillText';
 
 const Skills = () => {
   return (
     <section
       id="skills"
       className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
-      style={{ transform: "scale(0.9" }}
+      style={{ transform: 'scale(0.9' }}
     >
-      <div className="mt-96"><SkillText  /></div>
+      <div className="mt-96">
+        <SkillText />
+      </div>
 
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Skill_data.map((image, index) => (
@@ -42,7 +37,6 @@ const Skills = () => {
           />
         ))}
       </div>
-     
 
       <div className="w-full h-full absolute">
         <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
